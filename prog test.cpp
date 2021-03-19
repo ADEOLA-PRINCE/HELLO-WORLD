f8890da38a0929c94f31a66cd7bb81e15ab8b097
ADEOLA-PRINCE <78019794+ADEOLA-PRINCE@users.noreply.github.com>
2021-03-19 13:19:28 +0200

    Add files via upload

---
+++ prog test.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* Variant No 1, student of group 110, Adeola Prince Oluwajuwon*/
+int main() 
+{
+	/* // Task No1, Write an arithmetic expressioon in algorithmic language and indicate the order of opertaions
+	 L= (sqrt(e^x -cos^4*(x^2*a^5)) +arctg^4*(a-x^5))/esqrt(|a+xc^4|) */
+	
+	
+	
+	
+	
+	
+	
+	/* // Task No2, task text. write the condition that is true when 
+	a)only one of the numbers A and B is even
+	b)each of the numbers A,B,C is a multiple of three.*/
+	
+	int answer,A,B,C;
+	
+	cout << "Please enter THREE postive integer numbers: " << endl;
+	cin >> A,B,C;
+	 answer = A % 2; 
+	 A,B,C;
+	if (answer==0) {
+		cout<<  " True " << answer<< endl; 
+		
+	}
+	
+	
+	
+	//  Task No 4; Create an array of 14 integer elements. Find the number of elements that are even by value
+	
+	int n[14], even;
+	
+	cout << "Values of n" << i<< ndl;
+	for (int i=0;i<=13;i++){
+		cout<< i<< endl;
+	}
+	if (i=0){ 
+	even = i%2;
+	cout<< "the even numbers are: " << even << endl;
+		
+	}
+	
+	
+	return 0;
+}
+
+	
+
